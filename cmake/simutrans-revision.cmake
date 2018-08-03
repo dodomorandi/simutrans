fb19bf0416a88b7bdd9e21b614982699ce8c178c
ceeac <ceeac@users.noreply.github.com>
2018-08-03 10:27:33 +0200

    Add code for pulling revision information from git/svn

---
+++ cmake/simutrans-revision.cmake
@@ -0,0 +1,36 @@
+#
+# This file is part of the Simutrans project under the artistic licence.
+# (see licence.txt)
+#
+
+find_package(Subversion)
+find_package(Git)
+
+
+if (Subversion_FOUND OR Git_FOUND)
+	option(SIMUTRANS_WITH_REVISION "Build executable with SVN revision information" ON)
+endif (Subversion_FOUND OR Git_FOUND)
+
+
+if (SIMUTRANS_WITH_REVISION)
+	# We have to try Git first because Subversion_WC_INFO does not fail silently if
+	# this repository is not a Subversion repository
+	if (GIT_FOUND)
+		execute_process(COMMAND ${GIT_EXECUTABLE} describe --tags --always HEAD
+			OUTPUT_VARIABLE SIMUTRANS_WC_REVISION ERROR_QUIET OUTPUT_STRIP_TRAILING_WHITESPACE)
+	endif (GIT_FOUND)
+
+	if (NOT SIMUTRANS_WC_REVISION AND Subversion_FOUND)
+		Subversion_WC_INFO(${CMAKE_SOURCE_DIR} SIMUTRANS)
+	endif ()
+
+	if (SIMUTRANS_WC_REVISION)
+		message(STATUS "Configuring Simutrans ${SIMUTRANS_WC_REVISION} ...")
+		target_compile_definitions(simutrans PRIVATE REV=${SIMUTRANS_WC_REVISION})
+	else ()
+		message(WARNING "Could not find revision information because this repository "
+			"is neither a Subversion nor a Git repository. Revision information "
+			"will be unavailable.")
+		target_compile_definitions(simutrans PRIVATE REV="")
+	endif ()
+endif (SIMUTRANS_WITH_REVISION)
